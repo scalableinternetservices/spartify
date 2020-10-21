5336d5a46d0c5ff659f3ab3c0f6d3f8381327a27
Rohan Khajuria <rkhajuria99@gmail.com>
2020-10-21 13:15:27 -0400

    add migrations

---
+++ server/src/db/migrations/V1.1__Add_songs.sql
@@ -0,0 +1,2 @@
+insert into `song` (`name`, `artist`, `album`) values ('Sample Name 1', 'Sample Artist 1', 'Sample Album 1');
+insert into `song` (`name`, `artist`) values ('Sample Name 2', 'Sample Artist 2');
