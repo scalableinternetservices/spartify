8149354eac5b24def34f5fd29d530e230d253321
Kathy Daniels <danielskathyd@gmail.com>
2020-11-04 19:23:37 -0800

    add fetch party query

---
+++ web/src/view/page/fetchParty.ts
@@ -0,0 +1,60 @@
+import { gql } from '@apollo/client'
+
+export const fragmentSong = gql`
+  fragment Song on Song {
+    id
+    title
+    artist
+    album
+  }
+`
+
+export const fragmentVotedSong = gql`
+  fragment VotedSong on VotedSong {
+    id
+    song {
+      ...Song
+    }
+    count
+  }
+`
+
+export const fragmentPlayedSong = gql`
+  fragment PlayedSong on PlayedSong {
+    id
+    song {
+      ...Song
+    }
+    seq
+  }
+`
+
+export const fragmentParty = gql`
+  fragment Party on Party {
+    id
+    name
+    password
+    latestTime
+    currentSong {
+      ...Song
+    }
+    votedSongs {
+      ...VotedSong
+    }
+    playedSongs {
+      ...PlayedSong
+    }
+  }
+`
+
+export const fetchParty = gql`
+  query FetchParty($partyName: String!, $partyPassword: String) {
+    party(partyName: $partyName, partyPassword: $partyPassword) {
+      ...Party
+    }
+  }
+  ${fragmentParty}
+  ${fragmentSong}
+  ${fragmentVotedSong}
+  ${fragmentPlayedSong}
+`
